033ef951f97ccb95ec4a430260d44ab7fe6731db
tifanyh <341118578@gapps.yrdsb.ca>
2023-11-28 12:54:01 -0500

    change world size for image

--- MyWorld.java
+++ MyWorld.java
@@ -18,7 +18,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1, false);
+        super(600, 390, 1, false);
         
         //Create the elephant object.
         Elephant elephant = new Elephant();
